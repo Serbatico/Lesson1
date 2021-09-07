0ba95da1532de92dee03432e9b59782a26232eb8
Serbatico <Serbatico@yandex.ru>
2021-09-07 22:26:01 +0300

    L1

---
+++ src/MoneyTransfer.java
@@ -0,0 +1,11 @@
+
+public class MoneyTransfer {
+
+    public static void main(String[] args) {
+        int price = 2000000000;
+        int count = 500000000;
+        int total = price * count;
+        System.out.println(total);
+    }
+}
+
